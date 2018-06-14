2aeca0a9c00bb76fc19b84c870c9f1331554fec5
Kamil Piekielny <sajlenc@gmail.com>
2018-06-14 19:51:25 +0200

    creating windows

---
+++ projects/windows/src/sample/Person.java
@@ -0,0 +1,56 @@
+package sample;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Person {
+    private StringProperty name;
+    private IntegerProperty age;
+    private IntegerProperty id;
+    private static int staticId = 1;
+
+    Person(String name, int age){
+        this.name = new SimpleStringProperty(name);
+        this.age = new SimpleIntegerProperty(age);
+
+        id = new SimpleIntegerProperty(staticId++);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public int getAge() {
+        return age.get();
+    }
+
+    public IntegerProperty ageProperty() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age.set(age);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+}
